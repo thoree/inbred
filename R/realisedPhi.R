71db8af79e8819696e6aca433f14dede30cf2ed1
Thore <thore.egeland@gmail.com>
2021-03-21 19:40:45 +0100

    added plot and return last simulation

--- R/realisedPhi.R
+++ R/realisedPhi.R
@@ -11,6 +11,7 @@
 #' @param level Double
 #' @param method Character 'parametric' or 'nonparametric'
 #' @param bca Logical
+#' @param plot Logical
 #' @return Returns a l
 #'
 #' @details The equations
@@ -32,22 +33,35 @@
 #' ped = quadHalfFirstCousins()
 #' ids = leaves(ped)
 #' ped = setMarkers(ped, locusAttributes = NorwegianFrequencies)
-#' N = 400
-#' B = 400
+#' N = 1
+#' B = 100
 #' seed = 17
 #' bca = FALSE
+#' plot = T
 #' date()
+#' par(mfrow = c(1,2))
 #' resParametric = realisedPhi(ped = ped, ids = ids, N = N, B = B,
-#'                             method = "parametric", seed = seed, level = 0.95, bca = bca)
+#'                             method = "parametric", seed = seed,
+#'                             level = 0.95, bca = bca, plot = plot)
+#' foo = resParametric$lastBoot
+#' phi.hat = 0.25*foo$k1+0.5*foo$k2
+#' qqnorm(phi.hat, main = "", xlab ="")
+#'
+#' par(mfrow = c(1,2))
 #' resNonparametric = realisedPhi(ped = ped, ids = ids, N = N, B = B,
-#'                             method = "nonparametric", seed = seed, level = 0.95, bca = bca)
+#'                             method = "nonparametric", seed = seed, level = 0.95,
+#'                             bca = bca, plot = plot)
+#' foo = resNonparametric$lastBoot
+#' phi.hat = 0.25*foo$k1+0.5*foo$k2
+#' qqnorm(phi.hat, main = "", xlab = "")
+#'
 #' res = rbind(resParametric[N+1,], resNonparametric[N+1,] )
 #' rownames(res) = c("parametric", "nonparametric")
 #' round(res,4)
 #' date()
 #'
 realisedPhi <- function(ped, ids = NULL, N = 2, B = 2,
-                        seed = NULL, level = 0.95, method = "parametric", bca = FALSE){
+                        seed = NULL, level = 0.95, method = "parametric", bca = FALSE, plot = F){
   if(!is.null(seed))
     set.seed(seed)
   n = nMarkers(ped)
@@ -60,7 +74,8 @@ realisedPhi <- function(ped, ids = NULL, N = 2, B = 2,
       sim = markerSim(ped, N = n, verbose = F, ids = ids, seed = seeds[j])
       kappa.realised[[j]] = ibdEstimate(sim, ids = ids, param = "kappa", verbose = F)
       phi.realised[j] = 0.25*kappa.realised[[j]]$k1 + 0.5*kappa.realised[[j]]$k2
-      boot1 = ibdBootstrap(sim, ids = ids, kappa = kappa.realised[[j]],  N = B, plot = F, method = method)
+      boot1 = ibdBootstrap(sim, ids = ids, kappa = kappa.realised[[j]],
+                           N = B, method = method, plot = plot)
       phis = 0.25*boot1[,2] + 0.5*boot1[,3]
       phi.hat[j] = mean(phis)
       skew[j] = skewness(phis)
@@ -73,7 +88,7 @@ realisedPhi <- function(ped, ids = NULL, N = 2, B = 2,
                phi.pedigree = phi.pedigree, upper = CI[,2], coverage)
   res2 = rbind(res1, apply(res1, 2, mean))
   rownames(res2) = c(paste0("sim", 1:N), "average")
-  as.data.frame(res2)
+  list(tab = as.data.frame(res2), lastBoot = boot1)
 }
 
 ciRealised = function(x, level = 0.95, bca = FALSE){
